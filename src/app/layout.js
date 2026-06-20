dbee5e74ae98f4d6017468f795725c8023f5fd5e
Kazi Abdullah Fuad <kaziabdullahfuad@gmail.com>
2026-06-21 04:47:03 +0600

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Footer from "@/components/shared/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,10 +20,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      lang="en" data-theme='light'
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased` }
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+
+      <body className="min-h-full flex flex-col">
+        
+        <main>
+           {children}
+        </main>
+
+        <Footer></Footer>
+      
+      </body>
+
     </html>
   );
 }
