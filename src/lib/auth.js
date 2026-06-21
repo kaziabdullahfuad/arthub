29e37643e291efe503fd26b3799be4b895dd2660
Kazi Abdullah Fuad <kaziabdullahfuad@gmail.com>
2026-06-21 08:02:59 +0600

    Better auth initial setup done

---
+++ src/lib/auth.js
@@ -0,0 +1,19 @@
+import dns from "node:dns/promises";
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
+const client = new MongoClient(process.env.MONGODB_URI);
+const db = client.db(process.env.DB_NAME);
+
+export const auth = betterAuth({
+  emailAndPassword: { 
+    enabled: true, 
+  }, 
+  database: mongodbAdapter(db, {
+    // Optional: if you don't provide a client, database transactions won't be enabled.
+    client
+  }),
+});
